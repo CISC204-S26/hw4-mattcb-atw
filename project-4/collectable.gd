69b08fd694a631c661234fb5f13b9003cc6d1f28
mattcb-eng <mattcb@udel.edu>
2026-04-20 08:52:23 -0400

    First map

--- project-4/collectable.gd
+++ project-4/collectable.gd
@@ -6,6 +6,7 @@ var nearby_interactables = []
 func _on_interaction_detector_area_entered(area: Area2D) -> void:
 	area.set_active(true)
 	nearby_interactables.append (area)
+	print("eneterd")
 
 
 func _on_interaction_detector_area_exited(area: Area2D) -> void:
