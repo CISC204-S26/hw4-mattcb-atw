4a50e0423522f1191c04b84c8705679e43381357
oofski9-cyber <oofski9@gmail.com>
2026-04-27 12:09:53 -0400

    Trigger for second side room

--- project-4/Door.gd
+++ project-4/Door.gd
@@ -7,11 +7,8 @@ func _on_body_entered(body):
 	if body.name == "Player":
 		if is_final_door:
 			if GameState.side_room_1_done and GameState.side_room_2_done:
-				call_deferred("_change_scene")
+				get_tree().change_scene_to_file(next_scene)
 			else:
-				print("The final room is locked. Explore both side rooms first.")
+				print("Final room locked")
 		else:
-			call_deferred("_change_scene")
-
-func _change_scene():
-	get_tree().change_scene_to_file(next_scene)
+			get_tree().change_scene_to_file(next_scene)
