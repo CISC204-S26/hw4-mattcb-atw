16dc9f5494a159ba31611a2d6f331dd31220ed4e
oofski9-cyber <oofski9@gmail.com>
2026-04-26 23:11:54 -0400

    Added functional door back to main

--- project-4/Door.gd
+++ project-4/Door.gd
@@ -7,8 +7,11 @@ func _on_body_entered(body):
 	if body.name == "Player":
 		if is_final_door:
 			if GameState.side_room_1_done and GameState.side_room_2_done:
-				get_tree().change_scene_to_file(next_scene)
+				call_deferred("_change_scene")
 			else:
 				print("The final room is locked. Explore both side rooms first.")
 		else:
-			get_tree().change_scene_to_file(next_scene)
+			call_deferred("_change_scene")
+
+func _change_scene():
+	get_tree().change_scene_to_file(next_scene)
